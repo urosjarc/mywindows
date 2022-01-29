119f280f1564ac5902ca8a6871628534f3bdada5
urosjarc <jar.fmf@gmail.com>
2022-01-29 07:12:51 +0100

    Adding b3

--- main.ps1
+++ main.ps1
@@ -1,19 +1,24 @@
 # Set variables
 $TMP_DIR = ".\tmp"
-$BUGN_VERSION = "v9.0.2"
+$APPS_DIR = "$env:USERPROFILE\APPS"
+$B3_VERSION = "0.7"
 $GIT_USER = "urosjarc"
 $GIT_EMAIL = "jar.fmf@gmail.com"
 
+echo $APPS_DIR
+
 # INIT
 Remove-Item -Recurse -Force $TMP_DIR
+Remove-Item -Recurse -Force $APPS_DIR
 mkdir -Force $TMP_DIR
+mkdir -Force $APPS_DIR
 
 # DEPENDENCIES
 .\scripts\deps.ps1
 
-# INSTALL
+# # INSTALL
 Get-WindowsUpdate -AcceptAll -Install
-.\scripts\exe.ps4
+.\scripts\exe.ps1
 .\scripts\chocolatey.ps1
 .\scripts\npm.ps1
 .\scripts\pip.ps1
@@ -22,6 +27,4 @@ Get-WindowsUpdate -AcceptAll -Install
 Move-Item -Force .\config\Config.ini
 git config --global user.name "$GIT_USER"
 git config --global user.email "$GIT_EMAIL"
-Set-Location HKLM:\SOFTWARE\Policies\Microsoft\Windows
-New-ItemProperty -Force -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows" -Name "NoLockScreen" -Value 1 -PropertyType Dword
-Set-ItemProperty -Force -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows" -Name "NoLockScreen" -Value 1
+reg import .\config\registery.reg
--- scripts/exe.ps1
+++ scripts/exe.ps1
@@ -1,5 +1,5 @@
 
 # INSTALL BUGN
-Invoke-WebRequest "https://github.com/fuhsjr00/bug.n/archive/refs/tags/$BUGN_VERSION.zip" -OutFile "$TMP_DIR/bugn.zip"
-Expand-Archive "$TMP_DIR/bugn.zip" -DestinationPath $TMP_DIR
-Copy-Item -Force -Path "$TMP_DIR/bug*/bugn.exe" -Destination "$env:USERPROFILE\Desktop"
+Invoke-WebRequest "https://github.com/ritschmaster/b3/releases/download/$B3_VERSION/b3-$B3_VERSION.zip" -OutFile "$TMP_DIR/b3.zip"
+Expand-Archive "$TMP_DIR/b3.zip" -DestinationPath $TMP_DIR
+Copy-Item -Recurse -Force -Path "$TMP_DIR/b3-$B3_VERSION" -Destination "$env:USERPROFILE\APPS"
