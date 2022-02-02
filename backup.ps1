8d2af4d0e4164b6d061be17d176484d88787362c
urosjarc <jar.fmf@gmail.com>
2022-02-02 03:09:48 +0100

    Fix backup script for comparing existing with current.

--- backup.ps1
+++ backup.ps1
@@ -3,9 +3,12 @@
 . .\powershell\functions.ps1
 
 LOG_TITLE "BACKUP VISUAL STUDIO SETTINGS"
-$dir = "$ROOT_DIR\backup\visual_studio_code"
+$dir = "$TMP_DIR\visual_studio_code"
 RM_MK_DIR $dir
 COPY_ITEM "$env:USERPROFILE\AppData\Roaming\Code\User\keybindings.json" $dir
 COPY_ITEM "$env:USERPROFILE\AppData\Roaming\Code\User\settings.json" $dir
 LOG "Getting list of plugins"
 code --list-extensions | % { "code --install-extension $_" } > $dir\install_extensions.ps1
+
+meld $dir "$BACKUP_DIR\visual_studio_code"
+
