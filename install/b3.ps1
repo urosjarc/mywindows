54ff147649d1fa7a784ada9c9d0d121a819d6fff
urosjarc <jar.fmf@gmail.com>
2022-03-18 16:10:27 +0100

    Do not start b3 by default.

--- install/b3.ps1
+++ install/b3.ps1
@@ -1,5 +1,5 @@
 # Download from git
 $BIN = "$APPS_DIR\b3\b3-$B3_VERSION\bin"
 APPS_ZIP -app "b3" -url "https://github.com/ritschmaster/b3/releases/download/$B3_VERSION/b3-$B3_VERSION.zip"
-STARTUP_BAT -app "b3" -content "start /B /d $BIN b3.exe"
+# STARTUP_BAT -app "b3" -content "start /B /d $BIN b3.exe"
 COPY_ITEM "$FILES_DIR\b3.config" "$BIN/config"
