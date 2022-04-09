591e6115a1a3114295ec6b86a50e54a840bd8ada
urosjarc <jar.fmf@gmail.com>
2022-04-09 09:55:30 +0200

    Adding manual install for visual studio code

--- postinstall/manual_setup.ps1
+++ postinstall/manual_setup.ps1
@@ -1,11 +1,14 @@
-Start-Process "ms-settings:taskbar"
 Add-Type -AssemblyName Microsoft.VisualBasic
+
+Start-Process "ms-settings:taskbar"
 [Microsoft.VisualBasic.Interaction]::MsgBox('Deactivate taskbar items, auto hide taskbar... Then click OK','OKOnly,SystemModal,Information', 'Debloater')
 
 Start-Process "ms-settings:colors"
-Add-Type -AssemblyName Microsoft.VisualBasic
 $rez = [Microsoft.VisualBasic.Interaction]::MsgBox('Select windows colors... Then click OK','OKOnly,SystemModal,Information', 'Debloater')
 
 $id = Start-Process "ms-settings:nightlight"
-Add-Type -AssemblyName Microsoft.VisualBasic
 $rez = [Microsoft.VisualBasic.Interaction]::MsgBox('Activate night light... Then click OK','OKOnly,SystemModal,Information', 'Debloater')
+
+start "https://docs.unrealengine.com/5.0/en-US/setting-up-visual-studio-development-environment-for-cplusplus-projects-in-unreal-engine/"
+start "https://visualstudio.microsoft.com/vs/older-downloads/"
+$rez = [Microsoft.VisualBasic.Interaction]::MsgBox('Install Visual Studio... Then click OK','OKOnly,SystemModal,Information', 'Debloater')
